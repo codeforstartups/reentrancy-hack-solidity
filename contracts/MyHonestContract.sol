98397306371619cb5c36a6ec1b39d1181e95d8e8
codeforstartups <48567820+codeforstartups@users.noreply.github.com>
2022-01-19 13:16:04 +0530

    Two techniques for preventing Reentrancy Attack

--- contracts/MyHonestContract.sol
+++ contracts/MyHonestContract.sol
@@ -2,6 +2,9 @@
 
 pragma solidity ^0.8.0;
 
+//1. update your state variable before making call to another contract
+//2. using a modifier | only one funtion is executed at a time
+
 contract MyHonestContract {
     mapping(address => uint256) balances;
 
@@ -9,12 +12,25 @@ contract MyHonestContract {
         balances[msg.sender] += msg.value;
     }
 
-    function withdrawl(uint256 _amount) public {
+    bool internal locked;
+
+    modifier noReentrant() {
+        require(!locked, "no re-entrancy");
+        locked = true;
+        _;
+        locked = false;
+    }
+
+    function withdrawl(uint256 _amount) public noReentrant {
         require(balances[msg.sender] >= _amount);
+
+        balances[msg.sender] -= _amount; // updating the balance before sending ether
+
         (bool sent, ) = msg.sender.call{value: _amount}("");
+
         require(sent, "Failed to send ether");
 
-        balances[msg.sender] -= _amount;
+        // balances[msg.sender] -= _amount; this goes above before calling msg.sender.call
     }
 
     function getBalance() public view returns (uint256) {
